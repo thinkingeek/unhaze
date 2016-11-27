bd61ac7e445722f91b3438d221caf89c012754ca
Omkar Kulkarni <mail.omkar.kulkarni@gmail.com>
2016-11-27 15:11:10 +0530

    Second Commit

--- controllers/toolbar.js
+++ controllers/toolbar.js
@@ -1,10 +1,22 @@
 angular.module('unHaze')
     .controller('toolbarController',
-        ["$scope", "$rootScope", '$element', '$mdSidenav',
-            function($scope, $rootScope, $element, $mdSidenav){
+        ["$scope", "$rootScope", '$element', '$mdSidenav', '$mdDialog',
+            function($scope, $rootScope, $element, $mdSidenav, $mdDialog){
             $element.on('click', '.sidenav-toggle', function(){
                 $mdSidenav('left').toggle();
             });
+            $element.on('click', '.search-route', function(ev){
+                $mdDialog.show(
+                    $mdDialog.alert()
+                        .parent(angular.element(document.querySelector('.popup-container')))
+                        .clickOutsideToClose(true)
+                        .title('This is an alert title')
+                        .textContent('You can specify some description text in here.')
+                        .ariaLabel('Alert Dialog Demo')
+                        .ok('Got it!')
+
+                );
+            })
         }]
     );
 
