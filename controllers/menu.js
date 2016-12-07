736a41e0650146898921ec01758976e1da04881c
Omkar Kulkarni <mail.omkar.kulkarni@gmail.com>
2016-12-07 22:32:39 +0530

    Filled Sidebar Menu and Routed those menu to a category page

--- controllers/menu.js
+++ controllers/menu.js
@@ -3,6 +3,7 @@ angular.module('unHaze')
         ["$scope", '$http', '$element', '$state', '$mdSidenav', function($scope, $http, $element, $state, $mdSidenav){
             $http.get('data/menu.json').success(function(data){
                 $scope.menu = data;
+                $element.addClass('loaded');
             });
             $scope.stateName = $state.current.name;
             $scope.$state = $state;
